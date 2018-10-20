fb480e2d768ae5a4dbc4962113414f45896a404e
john murray <42979559+johnmurray2006@users.noreply.github.com>
2018-10-20 10:14:59 -0600

    New c# projetc

---
+++ MathAndComparisionOperations/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisionOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number");
+            string yourNumber = Console.ReadLine();
+            long yrNumber = Convert.ToInt32(yourNumber);
+            long product = yrNumber * 50;
+            Console.WriteLine("Your number times 50 is " + product);
+            Console.ReadLine();
+
+            Console.WriteLine("Pick a number");
+            string yourNumberOne = Console.ReadLine();
+            var yrNumberOne = Convert.ToInt32(yourNumberOne);
+            var total = yrNumberOne + 25;
+            Console.WriteLine("Your number plus twenty-five is " + total);
+            Console.ReadLine();
+
+            Console.WriteLine("Pick a number");
+            string yourNumberTwo = Console.ReadLine();
+            double yrNumberTwo = Convert.ToInt32(yourNumberTwo);
+            double quotianted = yrNumberTwo / 12.5;
+            Console.WriteLine("Your number divided by twelve and a half is  " + quotianted);
+            Console.ReadLine();
+
+            Console.WriteLine("Pick a number");
+            string yourNumberThree = Console.ReadLine();
+            double yrNumberThree = Convert.ToInt32(yourNumberThree);
+            bool isRight = yrNumberThree >= 50;
+            Console.WriteLine(isRight);
+            Console.ReadLine();
+
+            Console.WriteLine("Pick a number");
+            string yourNumberFour = Console.ReadLine();
+            int yrNumberFour = Convert.ToInt32(yourNumberFour);
+            int quotiantedOne = yrNumberFour / 7;
+            Console.WriteLine("Your number divided by seven " + quotiantedOne );
+            Console.ReadLine();
+            int remainder = (yrNumberFour % 7);
+            Console.WriteLine("The remainder is " + remainder);
+            Console.ReadLine();
+
+
+
+            //int combined = total + otherTotal;
+            //Console.WriteLine(" " + total.ToString());
+            //Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ Variables_and_Data_Types/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_and_Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is some simple code
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is:" + yourName);
+            // Console.ReadLine();
+            // Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            // Console.WriteLine("Your favorite number plus 5 is:" + total);
+            // Console.ReadLine();
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperture = -23;
+            char questionMark = '\u2013';
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+
+            decimal moneyInBank = 100.5m;
+            double heightInCentimeters = 211.30202092;
+            float secondsLeft = 2.62f;
+            short tempertureOnMars = -341;
+            string myName = "John";
+
+        }
+    }
+}

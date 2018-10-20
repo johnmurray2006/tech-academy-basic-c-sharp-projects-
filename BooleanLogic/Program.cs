fc3b65a736e8226221270ffecba8b0ddd5de64d0
john murray <42979559+johnmurray2006@users.noreply.github.com>
2018-10-20 14:41:50 -0600

    Boolean Logic project

---
+++ BooleanLogic/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && false);This is the and operator
+            //Console.ReadLine();
+            //Console.WriteLine(true || false); This is the or operator
+            //Console.WriteLine(true == false); Equals operator
+            //Console.WriteLine(true != false); Does not equal
+            // Console.WriteLine(true ^ false) This is the x or operator
+            Console.WriteLine("We have a few quetions for you to answer to see if Acme Insurance can offer you car insurance. ");
+            Console.ReadLine();
+            Console.WriteLine("What is your age");
+            string yourAge = Console.ReadLine();
+            int yrAge = Convert.ToInt32(yourAge);
+            Console.ReadLine();
+            Console.WriteLine("Have you ever had a DUI before? Answering true means no DUI's(true or false)");
+            string yourDui= Console.ReadLine();
+            bool a = yourDui == "true";
+            bool b = yourDui == "false";
+            
+              
+   
+            Console.WriteLine("How many speeding tickets do you have.");
+            string yourSpeeding = Console.ReadLine();
+            int yrSpeeding= Convert.ToInt32(yourSpeeding);
+            bool ageDriver = (yrAge >= 15);
+            bool ticketsDriver = (yrSpeeding < 3);
+            bool yrDui = (a || b);
+            Console.WriteLine("Quilified for our great car insurance.");
+
+
+            Console.WriteLine(ageDriver && yrDui && ticketsDriver);
+
+
+            Console.ReadLine();
+           
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ EmployeeSalaryComparision/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSalaryComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program.");
+            Console.ReadLine();
+            Console.WriteLine("Person1");
+            Console.ReadLine();
+            Console.WriteLine("Person1 makes 17 dollars a hour");
+            var dolPerHour = 17;
+            Console.ReadLine();
+            Console.WriteLine("Person1 works thirty-six hours per week.");
+            var hoursPerWeek = 36;
+            var product = dolPerHour * hoursPerWeek;
+            var yearlySalary = product * 52;
+            Console.ReadLine();
+            Console.WriteLine("Person1 weekly salary is " +  product);
+            Console.ReadLine();
+            Console.WriteLine("Person1 yearly salary is " + yearlySalary);
+            Console.ReadLine();
+
+
+            Console.WriteLine("Person2");
+            Console.ReadLine();
+            Console.WriteLine("Person2 makes 19 dollars a hour");
+            var dolPerHourA = 19;
+            Console.ReadLine();
+            Console.WriteLine("Person2 works forty per week.");
+            var hoursPerWeekA = 40;
+            var productA = dolPerHourA * hoursPerWeekA;
+            var yearlySalaryA = productA * 52;
+            Console.ReadLine();
+            Console.WriteLine("Person2 weekly salary is " + productA);
+            Console.ReadLine();
+            Console.WriteLine("Person2 yearly salary is " + yearlySalaryA);
+            Console.ReadLine();
+            Console.WriteLine("Does Person1 make more money than Person2");
+            Console.ReadLine();
+            Console.WriteLine("False");
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

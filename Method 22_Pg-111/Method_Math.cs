89eafc39a9440b88c551bb9e3f684d0fe9b94733
john murray <42979559+johnmurray2006@users.noreply.github.com>
2018-10-29 11:27:06 -0600

    Another one done

---
+++ Method 22_Pg-111/Method_Math.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_22_Pg_111
+{
+    public class Method_Math
+    {
+        public int result { get; set; }
+        public int result1 { get; set; }
+        public int result2 { get; set; }
+        public void Caller(int num1, int num2)
+        { 
+            if (num1 + num2 > 100)
+            {
+                result = (num1 + num2);
+            }
+            else if (num1 + num2 < 100)
+            {
+                result = 100;
+                
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        public void Caller1(int num3, int num4)
+        {
+            if (num3 * num4 > 100)
+            {
+                result1 = (num3 * num4);
+            }
+            else if (num3 * num4 < 100)
+            {
+                result = 100;
+              
+
+            }
+            Console.WriteLine(result1);
+            Console.ReadLine();
+        }
+           public void Caller2(int num5, int num6)
+        { 
+            if (num5 / num6 > 10)
+            {
+                result2 = (num5 / num6);
+            }
+            else if (num5 / num6 < 10)
+            {
+                result2 = 10;
+
+            }
+            Console.WriteLine(result2);
+            Console.ReadLine();
+        }
+        
+    }
+}
+   
+   
+
---
+++ Method 22_Pg-111/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_22_Pg_111
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Method_Math math = new Method_Math();
+            math.Caller(88, 76);
+            math.Caller1(77, 2);
+            math.Caller2(66, 7);
+           //Method 1
+            Console.WriteLine("Pick a number.");
+            string userNum1= Console.ReadLine();
+            int num1 = Convert.ToInt32(userNum1);
+            Console.WriteLine("Pick a number.");
+            string userNum2 = Console.ReadLine();
+            int num2=Convert.ToInt32(userNum2);
+            math.Caller( num1, num2);
+            // Method 2
+            Console.WriteLine("Pick a number.");
+            string userNum3 = Console.ReadLine();
+            int num3 = Convert.ToInt32(userNum3);
+            Console.WriteLine("Pick a number.");
+            string userNum4 = Console.ReadLine();
+            int num4 = Convert.ToInt32(userNum4);
+            math.Caller1(num3, num4);
+            // Method 3
+            Console.WriteLine("Pick a number.");
+            string userNum5 = Console.ReadLine();
+            int num5 = Convert.ToInt32(userNum5);
+            Console.WriteLine("Pick a number.");
+            string userNum6 = Console.ReadLine();
+            int num6 = Convert.ToInt32(userNum6);
+            math.Caller2(num5, num6);
+
+
+        }
+    }
+}
+

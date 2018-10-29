195ec622effd3f7abcff7debf2133d85a5aebcdd
john murray <42979559+johnmurray2006@users.noreply.github.com>
2018-10-29 14:18:40 -0600

    Another 1

---
+++ Project1/Step1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class Step1
+    {
+
+       
+        public int result { get; set; }
+       
+        public void Move1(int num1)
+        {
+        
+            Console.WriteLine(num1 * 200);
+            Console.ReadLine();
+        }
+        public void Move1(decimal num2)
+        {
+            
+            Console.WriteLine(num2 * 222);
+            Console.ReadLine();
+
+        }
+        public void Move1(string num3)
+
+        {
+            int num4 = Convert.ToInt32(num3);
+            Console.WriteLine(num4 * 23);
+            Console.ReadLine();
+
+        }
+
+
+    }
+}

b540fd24b2d3ded339cfbd0beb45fb59eb0c0709
john murray <42979559+johnmurray2006@users.noreply.github.com>
2018-10-29 15:10:43 -0600

    Optional parameter

---
+++ Methods2/Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Class2
+    {
+        public void Happy1(int num1, int optionalint=10)
+        {
+            Console.WriteLine(num1 - optionalint);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Methods2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 cl2 = new Class2();
+            Console.WriteLine();
+            Console.WriteLine("Pick a number.");
+            string userNum1 = Console.ReadLine();
+            int num1 = Convert.ToInt32(userNum1);
+            Console.WriteLine("Pick a number.");
+            string userNum2 = Console.ReadLine();
+            if (string.IsNullOrEmpty(userNum2))
+            {
+                cl2.Happy1(num1);
+            }
+            else
+            {
+                int num2 = Convert.ToInt32(userNum2);
+                cl2.Happy1(num1, num2);
+            }
+        }
+    }
+}

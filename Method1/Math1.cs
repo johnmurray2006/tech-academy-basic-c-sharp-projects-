bcd28dce4e812e54d056e61f287e80c2d89ad285
john murray <42979559+johnmurray2006@users.noreply.github.com>
2018-10-29 12:55:16 -0600

    Math1 to Github

---
+++ Method1/Math1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method1
+{
+    public class Math1
+
+    {   public int result { get; set; }
+        public int result1 { get; set; }
+        public void Hello(int num1, int num2)
+          {
+            if (num1 *22> 200)
+            {
+                result = (num1 * 22); 
+            }
+            else if (num1 *22< 200)
+            {
+                result = 200;
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+            Console.WriteLine(num2);
+            Console.ReadLine();
+        }
+    }       
+}
---
+++ Method1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math1 m1 = new Math1();
+            m1.Hello(13, 44);
+            
+
+        }
+    }
+}

d15433b378265e48f218bafac6ab85db12ae01e2
john murray <42979559+johnmurray2006@users.noreply.github.com>
2018-11-01 13:05:48 -0600

    EnumStep135

---
+++ EnumStep135/Enmu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumStep135
+{
+    public enum DaysofWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+    }
+}
---
+++ EnumStep135/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumStep135
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("What day of the week is it?");
+                string userDay = Console.ReadLine();
+               // int underlying = Convert.ToInt32(DaysofWeek.Thursday);
+               // Console.WriteLine(underlying);
+                DaysofWeek day = (DaysofWeek)Enum.Parse(typeof(DaysofWeek),userDay, true);
+                Console.WriteLine();
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+               
+                {
+                    Console.WriteLine("Please enter the correct spelling of the day of the week it is.");
+                }
+              
+            }
+
+
+            {
+                Console.ReadLine();
+            }
+            
+
+        }
+
+
+    
+    }
+}

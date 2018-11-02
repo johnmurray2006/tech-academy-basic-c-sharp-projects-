407fd934a5309280beb6e99c3bb4d2373fbc0bd9
john murray <42979559+johnmurray2006@users.noreply.github.com>
2018-11-02 10:19:10 -0600

    Stucts Step 137

---
+++ Struct1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct1
+{
+    class Program
+    {
+        public struct Number
+        {
+            public decimal a;
+            public Number(decimal b1)
+            {
+                a = b1;
+            }
+        }
+        static void Main(string[] args)
+
+        {
+
+            Number number1;
+            number1.a = 10;
+            Console.WriteLine(number1.a);
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
+

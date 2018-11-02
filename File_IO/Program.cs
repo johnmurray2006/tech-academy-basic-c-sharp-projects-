f66f58931ce8074bcdeba103d6f45e63ffce1f5c
john murray <42979559+johnmurray2006@users.noreply.github.com>
2018-11-02 11:55:27 -0600

    File I/O Step154

---
+++ File_IO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number to be logged into a text file.");
+            string text = Console.ReadLine() ;
+            File.WriteAllText("C:\\Users\\John Murray\\source\\repos\\fileio.txt", text);
+            string fileRead = File.ReadAllText("C:\\Users\\John Murray\\source\\repos\\fileio.txt");
+            Console.WriteLine("Number contained in file " + " " + fileRead);
+            Console.ReadLine();
+        }
+    }
+}

f6de43fdd54b6e44e9c74174e57f9d40569fdb7e
john murray <42979559+johnmurray2006@users.noreply.github.com>
2018-11-05 08:57:54 -0700

    TryCatch_Step165

---
+++ TryCatchStep165/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchStep165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+
+            {
+                Console.WriteLine("What is your age?");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age <= 0) { throw new FormatException(); }
+                if (age > 999999999) { throw new Exception(); }
+                int year = 2018 - age;
+                Console.WriteLine(year);
+                Console.ReadLine();
+             }
+                catch (FormatException ex)
+            {
+                Console.WriteLine("Please do not enter zero or negative numbers.");
+            }
+                catch (Exception ex)
+
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}

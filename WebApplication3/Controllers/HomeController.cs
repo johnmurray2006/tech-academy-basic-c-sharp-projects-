86dc4dec56868202f20c0fc69efe933992afcd31
john murray <42979559+johnmurray2006@users.noreply.github.com>
2018-11-19 15:26:43 -0700

    Car Insurance Project

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            User user = new User();
+            user.Id = 1;
+            user.firstName = "John";
+            user.lastName = "Murray";
+            user.Age = 44;
+            return View(user);
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Hello";
+
+            return View();
+        }
+
+        public ActionResult Contact(int id )
+        {
+            ViewBag.Message = id=0;
+
+            return View();
+        }
+    }
+}
